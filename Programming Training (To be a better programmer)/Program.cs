ee4a7159566fc2da033f955a99718bda4f4d6e2f
Aldrich Roque <Aldrich.Roque@ogglobal.net>
2024-10-30 09:01:58 +0800

    Add Asynchronous programming sample and add odulus operator

--- Programming Training (To be a better programmer)/Program.cs
+++ Programming Training (To be a better programmer)/Program.cs
@@ -2,10 +2,25 @@
 
 class Program
 {
+    private static Func<int, bool> _odd = (int x) =>  x % 2 == 0;
+    private static Func<int, bool> _even = (int x) => x % 2 == 0;
+
+    private static Predicate<int> _predicateEven = (int x) => x % 2 == 0;
     static void Main()
     {
+        Console.WriteLine("Enter if you want to print even or odd number:");
+        string input = Console.ReadLine();
 
-        UpdateUserImmutable();
+       IEnumerable<int> nums = Enumerable.Range(1, 100)
+                                         .Where(x => 
+                                         input == "even"
+                                         ? _predicateEven(x)
+                                         : _odd(x)
+                                         );
+        foreach(int num in nums)
+        {
+            Console.WriteLine(num);
+        }
     }
 
     public static void UpdateUserImmutable()
---
+++ Programming Training (To be a better programmer)/Static Data/AsyncronousProgramming.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Training__To_be_a_better_programmer_.Static_Data
+{
+    public static class AsyncronousProgramming
+    {
+        public static  void PerformAsyncTask()
+        {
+            Console.WriteLine("Task started...");
+
+            // Simulate a delay
+             Task.Delay(10000);
+
+            Console.WriteLine("Task finished after delay.");
+        }
+    }
+}
